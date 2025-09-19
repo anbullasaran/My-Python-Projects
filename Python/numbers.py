bd247a697af80b6c9660d0375eb128948dd8eda7
anbullasaran <anbullasaran@gmail.com>
2025-09-18 22:19:50 -0700

    changes done in mumbers.py file

--- Python/numbers.py
+++ Python/numbers.py
@@ -1,1 +1,16 @@
-print("vinayaka im sorry and please be with me")
+# fprint numbers from 0 to 100 with a difference of 10
+
+'''i=0
+while i<=100:
+    print (i,end=",")
+    i=i+10
+    '''
+
+
+# factorial of a number
+
+'''i=0
+fact=1
+for i in range(1,6):
+    fact=fact*i
+print("The factorial of", i, "is : ", fact)'''
